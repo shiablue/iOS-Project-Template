94fc54ef6f5ba29970d6658c6fd428b7de914e09
BB9z <bb9z@live.com>
2013-03-20 13:33:43 +0800

    Template @20140508. v3.0

---
+++ App/General/MBMultiLineLabel.h
@@ -0,0 +1,17 @@
+/*!
+    MBMultiLineLabel
+
+    Copyright © 2014 Chinamobo Co., Ltd.
+    https://github.com/Chinamobo/iOS-Project-Template
+
+    Apache License, Version 2.0
+    http://www.apache.org/licenses/LICENSE-2.0
+ */
+#import <UIKit/UIKit.h>
+
+/**
+ 这个类为了解决 Auto Layout label 自适应高度的问题
+ */
+@interface MBMultiLineLabel : UILabel
+
+@end
